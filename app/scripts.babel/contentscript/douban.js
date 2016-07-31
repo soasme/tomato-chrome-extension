3013dc3bf1e609eb6700defadf65c5c8e652758b
soasme <soasme@gmail.com>
2016-07-31 16:22:12 +0800

    Add interaction for douban.

--- app/scripts.babel/contentscript/douban.js
+++ app/scripts.babel/contentscript/douban.js
@@ -53,53 +53,108 @@ var source = `
   }
 </style>
 <div class="mod-hd">
-  <a class="redbutt j a_collect_btn rr" href="javascript:;" >
+  <a href="{{addResourceUrl}}" class="redbutt j add-resource-btn">
     <span>我来贴链接</span>
   </a>
   <h2>
     <span>番茄</span>· · · · · ·
     <span class="pl">
-      (<a href="{{resources_url}}">全部 {{resources_count}} 条</a>)
+      (<a href="{{resourcesUrl}}">查看全部</a>)
     </span>
   </h2>
 </div>
 <div class="nav-tab">
   <div class="tabs-wrapper  line">
-    <a class="short-resource-tabs on-tab" href="hot" data-tab="hot">热门</a>
+    <a class="short-resource-tabs on-tab" href="javascript:;" data-tab="hot">热门</a>
     <span>/</span>
-    <a class="short-resource-tabs " href="new" data-tab="new">最新</a>
+    <a class="short-resource-tabs " href="javascript:;" data-tab="latest">最新</a>
     <span>/</span>
-    <a class="short-resource-tabs " href="follows" data-tab="follows">自己的</a>
+    <a class="short-resource-tabs " href="javascript:;" data-tab="user">自己的</a>
   </div>
 </div>
+
 <div class="indent" id="resource-list-wrapper">
-  <div class="resource-list hot noshow show">
-    <ul>{{#each hot_resources}}
-      <li class="resource-item">
-        <h3>
-          <span class="resource-vote">
-            <span class="vote-count">{{votes_count}}</span>
-            <a href="javascript:;" class="j vote-resource" data-resource-id="{{id}}">有用</a>
-          </span>
-          <span class="resource-info">
-            <a href="{{user.url}}">{{user.username}}</a>
-            <span>{{created_at}}</span>
-          </span>
-        </h3>
-        <p class="resource-content">
-          <a href="{{url}}">{{title}}</a>
-          <span>{{description}}</span>
-        </p>
-      </li>
-    {{/each}}</ul>
-  </div>
+  {{#each types}}
+    <div class="resource-list {{type}} noshow {{#if isShown}}show{{/if}}">
+      <ul>{{#each resources}}
+        <li class="resource-item">
+          <h3>
+            <span class="resource-vote">
+              <span class="vote-count">{{votes_count}}</span>
+              {{#if isVoted}}<span>已投票</span>{{/if}}
+              <a href="javascript:;" class="j vote-resource" data-resource-id="{{id}}">有用</a>
+            </span>
+            <span class="resource-info">
+              <a href="{{user.url}}">{{user.username}}</a>
+              <span>{{created_at}}</span>
+            </span>
+          </h3>
+          <p class="resource-content">
+            <a href="{{url}}">{{title}}</a>
+            <span>{{description}}</span>
+          </p>
+        </li>
+      {{/each}}</ul>
+    </div>
+  {{/each}}
 </div>
 `
 var template = Handlebars.compile(source)
-var data = {
-  hot_resources: getResourcesByISBN('whatever'),
-  resources_count: 100,
-  resources_url: 'https://tomato.today/subjects/1/resources' + '?icn=crx',
-}
-var $el = $(template(data))
-$el.insertAfter($("#db-tags-section"))
+var isbnMatch = $(".subject #info").html().match(/(97[89]\d{9}[\dXx])/)
+var isbn = isbnMatch[1]
+
+$.when(
+  getResourcesByISBN(isbn, 'latest', 5),
+  getResourcesByISBN(isbn, 'hot', 5),
+  getResourcesByISBN(isbn, 'user', 5)
+).done(function(
+  latestResources,
+  hotResources,
+  userResources
+) {
+  // build template data
+  var data = {
+    types: [
+      {
+        type: 'hot',
+        isShown: true,
+        resources: hotResources
+      }, {
+        type: 'latest',
+        isShown: false,
+        resources: latestResources
+      }, {
+        type: 'user',
+        isShown: false,
+        resources: userResources
+      }
+    ],
+    resourcesUrl: `https://tomato.today/isbn/${ isbn }/resources`,
+    addResourceUrl: `https://tomato.today/isbn/${ isbn }/resources`,
+  }
+
+  // render template
+  var $el = $(template(data))
+  $el.insertAfter($("#rec-ebook-section"))
+
+  // init tab
+  $(".short-resource-tabs").on('click', function(e) {
+    var type = $(e.target).data('tab')
+    $(".short-resource-tabs").removeClass('on-tab')
+    $(e.target).addClass('on-tab')
+    $(".resource-list ").removeClass('show')
+    $(`.resource-list.${ type }`).addClass('show')
+  })
+
+  // init vote
+  $(".vote-resource").on('click', function(e) {
+    var $this = $(e.target)
+    var resourceID = $this.data('resource-id')
+    voteResource(resourceID).done(function(voted) {
+      if (voted) {
+        $this.parent().append($("<span>已投票</span>"))
+        $this.remove()
+      }
+    })
+  })
+})
