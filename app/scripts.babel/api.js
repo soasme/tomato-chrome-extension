d9137e9b6923a64176cb9bd44f1af3dfa098cf7a
soasme <soasme@gmail.com>
2016-07-31 11:35:27 +0800

    add mock data for api.

---
+++ app/scripts.babel/api.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function getResourcesByISBN(isbn) {
+  return [
+    {
+      'id': 1,
+      'title': 'This is mocking data.',
+      'url': 'https://tomato.today',
+      'description': '老年人之间的小春日和，长情相伴，春种秋收。',
+      'created_at': '2016-07-06',
+      "votes_count": 10,
+      'user': {
+        'username': 'soasme',
+        'url': 'https://tomato.today/users/1/',
+      }
+    }
+  ]
+}
