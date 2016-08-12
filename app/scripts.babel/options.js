8b5269cadd40a0a51113a94c5beaf6e2f303d076
soasme <soasme@gmail.com>
2016-08-12 10:29:57 +0800

    handle login interation.

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,3 +1,56 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Option');
+
+var getLogin = () => {
+  var dfd = $.Deferred()
+  chrome.storage.sync.get(null, (storage) => {
+    if (storage.token) {
+      dfd.resolve(true)
+    } else {
+      dfd.resolve(false)
+    }
+  })
+  return dfd.promise()
+}
+
+$.when(
+  getLogin()
+).then(function(
+  isLogin
+) {
+  if (isLogin) {
+    $(".login").hide()
+    $(".logout").show()
+  } else {
+    $(".logout").hide()
+    $(".login").show()
+  }
+
+  $(".logout").on('click', function(e) {
+    chrome.extension.sendMessage({
+      action: "logout"
+    }, function(response) {
+      if (response.loggedOut) {
+        $(".logout").hide()
+        $(".login").show()
+      } else {
+        alert(response.message)
+      }
+    })
+  })
+
+  $(".login").on('click', function(e) {
+    chrome.extension.sendMessage({
+      action: "login"
+    }, function(response) {
+      if (response.loggedIn) {
+        $(".logout").show()
+        $(".login").hide()
+      } else {
+        alert(response.message)
+      }
+    })
+  })
+
+})
